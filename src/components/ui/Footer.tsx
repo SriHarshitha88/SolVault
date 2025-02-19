aba68a60c855a37e88298ddc6badf057e0e47f47
Harshitha <168890205+SriHarshitha88@users.noreply.github.com>
2025-02-19 20:16:00 +0530

    v2 Optimized Components

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-3 py-3 md:p-5 border-t border-[#27272a]">
+            <h3 className="text-center text-white spacing tracking-widest">
+                Designed & Developed by <span className="underline font-bold">Sri Harshitha J</span>
+            </h3>
+        </footer>
+    )
+}
+
+export default Footer;
